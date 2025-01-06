cc56a5ee66e6569ccdcc981e90ad0ffe20fbe095
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2025-01-06 16:46:44 -0500

    update

--- scripts/quad_planet.gd
+++ scripts/quad_planet.gd
@@ -10,7 +10,11 @@ extends MeshInstance3D
 
 
 func _ready() -> void:
-	pass
+	mesh = ArrayMesh.new()
+	var surface_array: Array = []
+	surface_array.resize(Mesh.ARRAY_MAX)
+	surface_array = generate_quadplanet_array(radius, marker.position, lod_distances)
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
 	
 	
 func _process(_delta: float) -> void:
@@ -18,17 +22,28 @@ func _process(_delta: float) -> void:
 
 
 #region Static functions
-static func generate_quadplane_array(relative_marker_pos: Vector3, lod_dist: Array[float]) -> Array[Array]:
-	var surface_array: Array[Array] = []
+static func generate_quadplanet_array(rad:float, marker_pos: Vector3, lod_dist: Array[float]) -> Array:
+	var surface_array: Array = []
 	surface_array.resize(Mesh.ARRAY_MAX)
-	surface_array[Mesh.ARRAY_VERTEX] = PackedVector3Array()
-	surface_array[Mesh.ARRAY_NORMAL] = PackedVector3Array()
-	surface_array[Mesh.ARRAY_INDEX] = PackedInt32Array()
-	var vertices: PackedVector3Array = surface_array[Mesh.ARRAY_VERTEX]
-	var normals: PackedVector3Array = surface_array[Mesh.ARRAY_NORMAL]
-	var indices: PackedInt32Array = surface_array[Mesh.ARRAY_INDEX]
+	var vertices: PackedVector3Array = PackedVector3Array()
+	var normals: PackedVector3Array = PackedVector3Array()
+	var indices: PackedInt32Array = PackedInt32Array()
 	
+	var marker_dist: float = Vector3.ZERO.distance_to(marker_pos)
 	
+	# Generate first plane verts
+	vertices.append(Vector3(rad, rad, rad))
+	vertices.append(Vector3(-rad, rad, rad))
+	vertices.append(Vector3(rad, -rad, rad))
+	vertices.append(Vector3(-rad, -rad, rad))
 	
+	indices.append_array([2, 1, 0])
+	indices.append_array([1, 2, 3])
+	
+	surface_array[Mesh.ARRAY_VERTEX] = vertices
+	#surface_array[Mesh.ARRAY_NORMAL] = normals
+	surface_array[Mesh.ARRAY_INDEX] = indices
+	print(surface_array[Mesh.ARRAY_VERTEX])
+	print(surface_array[Mesh.ARRAY_INDEX])
 	return surface_array
 #endregion
