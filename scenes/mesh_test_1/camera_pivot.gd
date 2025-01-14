2f8d982b4bf951046997e2c283f566a8287bd75b
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2025-01-14 16:57:43 -0500

    updates

--- scenes/mesh_test_1/camera_pivot.gd
+++ scenes/mesh_test_1/camera_pivot.gd
@@ -3,12 +3,17 @@ extends Node3D
 
 @export var speed: float = 5.0
 @export var zoom_scale: float = 1.0
-
+@export var arrow_keys: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var rotation_vec: Vector2 = Input.get_vector("left", "right", "down", "up")
+	var rotation_vec: Vector2
+	if not arrow_keys:
+		rotation_vec = Input.get_vector("left", "right", "down", "up")
+	else:
+		rotation_vec = Input.get_vector("l_a", "r_a", "d_a", "u_a")
 	rotation.y += rotation_vec.x * speed * delta
 	rotation.x += rotation_vec.y * speed * delta * -1
 	var zoom_vec: float = Input.get_axis("left_click", "right_click")
-	scale += Vector3.ONE * zoom_vec * zoom_scale * delta
+	if not arrow_keys:
+		scale += Vector3.ONE * zoom_vec * zoom_scale * delta
--- scripts/quad_planet.gd
+++ scripts/quad_planet.gd
@@ -10,76 +10,104 @@ extends MeshInstance3D
 
 
 func _ready() -> void:
-	mesh = ArrayMesh.new()
-	var surface_array: Array = []
-	surface_array.resize(Mesh.ARRAY_MAX)
-	surface_array = generate_quadplanet_array(radius, marker.position, lod_distances)
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	mesh = generate_mesh(radius, marker.position, lod_distances, noise)
+	print("Mesh Vert Count: ", mesh.surface_get_array_len(Mesh.ARRAY_VERTEX))
 	
 	
 func _process(_delta: float) -> void:
+	#mesh = generate_mesh(radius, marker.position, lod_distances, noise)
 	pass
 
 
 #region Static functions
-static func generate_quadplanet_array(rad:float, marker_pos: Vector3, lod_dist: Array[float]) -> Array:
-	var surface_array: Array = []
-	surface_array.resize(Mesh.ARRAY_MAX)
-	var vertices: PackedVector3Array = PackedVector3Array()
-	var normals: PackedVector3Array = PackedVector3Array()
-	var indices: PackedInt32Array = PackedInt32Array()
+static func generate_mesh(rad: float, marker_pos: Vector3, lod_dist: Array[float], noise: Noise) -> Mesh:
+	var st: SurfaceTool = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	for vert: Vector3 in generate_verts(rad, marker_pos, lod_dist, noise):
+		st.set_smooth_group(-1)
+		st.add_vertex(vert)
+	st.index()
+	st.generate_normals()
+	return st.commit()
+
+
+static func generate_verts(rad: float, marker_pos: Vector3, lod_dist: Array[float], noise: Noise) -> PackedVector3Array:
+	var verts: PackedVector3Array = PackedVector3Array()
+	var quads: Array[Array] = []
 	
-	var quad_indices: Array[Array] = []
+	var directions: Array[Vector3] = [
+		Vector3.UP,
+		Vector3.DOWN,
+		Vector3.LEFT,
+		Vector3.RIGHT,
+		Vector3.FORWARD,
+		Vector3.BACK
+	]
+	for dir: Vector3 in directions:
+		var plane: Array[Vector3]
+		# Turn this into an array of quads instead of a quad and then have a 
+		# new quads array that leaf nodes can be recursively added to while passing
+		# all the branch nodes to next loop
+		plane.assign(generate_cube_face(dir).map(
+					func(x: Vector3) -> Vector3: return x.normalized() * rad
+			))
+		quads.append_array(subdivide_face(marker_pos, lod_dist[0], plane))
 	
-	# Generate first plane verts
-	vertices.append(Vector3(rad, rad, rad))
-	vertices.append(Vector3(rad, -rad, rad))
-	vertices.append(Vector3(-rad, -rad, rad))
-	vertices.append(Vector3(-rad, rad, rad))
-	var quad: Array[int] = [0, 1, 2, 3]
-	
-	if quad.any(
-			func(x: int) -> bool:
-				return true if marker_pos.distance_to(vertices[x]) < lod_dist[0] else false
-	):
-		var new_quads: Array[Array] = []
-		var new_verts: PackedVector3Array = PackedVector3Array()
-		for i: int in quad:
-			new_verts.append(vertices[i].lerp(vertices[(i % 3) + 1], 0.50))
-		new_verts.append(quad.reduce(
-			func(accum: Vector3, x: int) -> Vector3:
-				return accum + vertices[x]
-		, Vector3.ZERO) / 4)
-		vertices.append_array(new_verts)
-		new_quads.append([
-			quad[0],
-			len(vertices) - 5,
-			len(vertices) - 1,
-			len(vertices) - 2
-		])
-		quad_indices.append_array(new_quads)
+	# Append quads as two triangles
+	for quad: Array[Vector3] in quads:
+		verts.append_array(PackedVector3Array([
+			quad[0], quad[1], quad[2],
+			quad[2], quad[3], quad[0]
+		]))
+	return verts
+
+
+static func subdivide_face(marker_pos: Vector3, dist: float, face: Array[Vector3]) -> Array[Array]:
+	if any_within_distance(marker_pos, dist, face):
+		var new_verts: Array[Vector3] = [
+			(face[0] + face[1]) / 2,
+			(face[1] + face[2]) / 2,
+			(face[2] + face[3]) / 2,
+			(face[3] + face[0]) / 2,
+			face.reduce(func(accum: Vector3, x: Vector3) -> Vector3: return accum + x, Vector3.ZERO) / 4
+		]
+		return [
+			[face[0], new_verts[0], new_verts[4], new_verts[3]],
+			[face[1], new_verts[1], new_verts[4], new_verts[0]],
+			[face[2], new_verts[2], new_verts[4], new_verts[1]],
+			[face[3], new_verts[3], new_verts[4], new_verts[2]]
+		]
 	else:
-		quad_indices.append(quad)
-	
-	for i_quad: Array[int] in quad_indices:
-		indices.append_array(i_quad.slice(0, 3))
-		indices.append_array(i_quad.slice(2, 4))
-		indices.append(i_quad[0])
-	
-	surface_array[Mesh.ARRAY_VERTEX] = vertices
-	#surface_array[Mesh.ARRAY_NORMAL] = normals
-	surface_array[Mesh.ARRAY_INDEX] = indices
-	print(surface_array[Mesh.ARRAY_VERTEX])
-	print(surface_array[Mesh.ARRAY_INDEX])
-	return surface_array
-	
-	
-## Appends value to array and returns index of new value.
-static func i_append(x: Variant, arr: Array) -> int:
-	arr.append(x)
-	return len(arr)
-	
-	
+		return [face]
+
+
+static func generate_cube_face(normal: Vector3) -> Array[Vector3]:
+	# Normalize the input vector to ensure we're working with a unit vector
+	normal = normal.normalized()
+	# Find a vector perpendicular to our normal vector
+	# We can do this by finding the cross product with any vector not parallel to normal
+	# Using UP vector (0,1,0) works in most cases, but we need a fallback
+	var right: Vector3
+	if abs(normal.dot(Vector3.UP)) < 0.999:
+		right = normal.cross(Vector3.UP).normalized()
+	else:
+		# If normal is parallel to UP, use FORWARD instead
+		right = normal.cross(Vector3.FORWARD).normalized()
+	# Get the up vector for our face by crossing normal with right
+	# This ensures our vectors are perfectly perpendicular
+	var up: Vector3 = normal.cross(right)
+	# Now we can construct our four corners
+	# The face will be centered at normal * 0.5 (half a unit from center)
+	var center: Vector3 = normal * 0.5
+	# Create the four corners by adding and subtracting right and up vectors
+	var corners: Array[Vector3] = []
+	corners.append(center + right * 0.5 + up * 0.5)  # Top-right
+	corners.append(center + right * 0.5 - up * 0.5)  # Bottom-right
+	corners.append(center - right * 0.5 - up * 0.5)  # Bottom-left
+	corners.append(center - right * 0.5 + up * 0.5)  # Top-left
+	return corners
+
+
 ## Returns [code]true[/code] if [param pos] is within [param dist] of any
 ## element in [param points].
 static func any_within_distance(pos: Vector3, dist: float, points: Array[Vector3]) -> bool:
