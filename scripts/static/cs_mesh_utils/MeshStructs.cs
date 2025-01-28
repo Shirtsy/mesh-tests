7b4c9fe2fccd946ebb2b4fec2ee35eee79cbd97f
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2025-01-28 12:25:28 -0500

    Progress

--- scripts/static/cs_mesh_utils/MeshStructs.cs
+++ scripts/static/cs_mesh_utils/MeshStructs.cs
@@ -1,42 +1,128 @@
+using Godot;
 using System;
-using Num = System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
 
 public readonly struct Vertex
 {
-    public readonly Num.Vector3 XYZ;
-    public readonly Num.Vector2 UV;
-    public readonly Num.Vector3 Normal;
-    public readonly int SmoothGroup = -1;
+    public readonly Vector3 XYZ;
+    public readonly Vector2 UV;
+    public readonly Vector3 Normal;
+    public readonly uint SmoothGroup;
 
-    public Vertex() {}
+    public Vertex(
+        Vector3 xyz,
+        Vector2 uv,
+        uint smoothGroup = uint.MaxValue)
+    {
+        XYZ = xyz;
+        UV = uv;
+        SmoothGroup = smoothGroup;
+    }
+
+    public static Vertex operator +(Vertex a, Vertex b) =>
+        new Vertex(a.XYZ + b.XYZ, a.UV + b.UV);
+
+    public static Vertex operator -(Vertex a, Vertex b) =>
+            new Vertex(a.XYZ + b.XYZ, a.UV + b.UV);
+
+    public static Vertex operator *(Vertex a, float b) =>
+        new Vertex(a.XYZ * b, a.UV * b);
+
+    public static Vertex operator /(Vertex a, float b) =>
+        new Vertex(a.XYZ / b, a.UV / b);
+}
+
+public readonly struct Tri
+{
+    private readonly Vertex _v1;
+    private readonly Vertex _v2;
+    private readonly Vertex _v3;
+
+    public Tri(Vertex v1, Vertex v2, Vertex v3)
+    {
+        _v1 = v1;
+        _v2 = v2;
+        _v3 = v3;
+    }
+
+    public Vertex this[int index] => index switch
+    {
+        0 => _v1,
+        1 => _v2,
+        2 => _v3,
+        _ => throw new IndexOutOfRangeException(
+            "Quad vertices must be accessed with index 0-2")
+    };
+
+    public IEnumerable<Vertex> Vertices
+    {
+        get
+        {
+            yield return _v1;
+            yield return _v2;
+            yield return _v3;
+        }
+    }
 }
 
 public readonly struct Quad
 {
-    private readonly Vertex[] _verts;
-    public readonly Vertex[] Verts
+    private readonly Vertex _v1;
+    private readonly Vertex _v2;
+    private readonly Vertex _v3;
+    private readonly Vertex _v4;
+
+    public Quad(Vertex v1, Vertex v2, Vertex v3, Vertex v4)
     {
-        get => _verts;
-        init
+        _v1 = v1;
+        _v2 = v2;
+        _v3 = v3;
+        _v4 = v4;
+    }
+
+    public Vertex this[int index] => index switch
+    {
+        0 => _v1,
+        1 => _v2,
+        2 => _v3,
+        3 => _v4,
+        _ => throw new IndexOutOfRangeException(
+            "Quad vertices must be accessed with index 0-3")
+    };
+
+    public IEnumerable<Vertex> Vertices
+    {
+        get
         {
-            if (value.Length != 4)
-            {
-                throw new ArgumentOutOfRangeException
-                (
-                    "Parameter index is out of range.", nameof(value)
-                );
-            }
-            else
-            {
-                _verts = value;
-            }
+            yield return _v1;
+            yield return _v2;
+            yield return _v3;
+            yield return _v4;
         }
     }
-    
-    public readonly (Vertex[], Vertex[]) Tris
+
+    public readonly (Tri, Tri) Tris
     {
         get =>
-            ([Verts[0], Verts[1], Verts[2]],
-            [Verts[2], Verts[3], Verts[0]]);
+            (new Tri(_v1, _v2, _v3),
+            new Tri(_v3, _v4, _v1));
+    }
+
+    public Quad[] Subdivide()
+    {
+        Vertex[] newV = [
+            (_v1 + _v2) / 2,
+            (_v2 + _v3) / 2,
+            (_v3 + _v4) / 2,
+            (_v4 + _v1) / 2,
+            (_v1 + _v2 + _v3 + _v4) / 4
+        ];
+        return [
+            new Quad(_v1, newV[0], newV[4], newV[3]),
+            new Quad(newV[0], _v2, newV[1], newV[4]),
+            new Quad(_v3, newV[2], newV[4], newV[1]),
+            new Quad(newV[2], _v4, newV[3], newV[4])
+        ];
     }
 }
---
+++ scripts/static/cs_mesh_utils/PlanetUtils.cs
@@ -0,0 +1,40 @@
+using Godot;
+
+public static class PlanetUtils : object
+{   
+    public static (Mesh Draw, Mesh Collider) GenerateMesh(
+        double radius,
+        Vector3 markerPos,
+        double[] lodDist,
+        Noise planetNoise,
+        double noiseMult)
+    {
+        Mesh ProcessVertArray(Vertex[] vertsArr)
+        {
+            SurfaceTool st = new SurfaceTool();
+            st.Begin(Mesh.PrimitiveType.Triangles);
+            foreach (Vertex vert in vertsArr)
+            {
+                st.SetSmoothGroup(vert.SmoothGroup);
+                st.SetUV(vert.UV);
+                st.AddVertex(vert.XYZ);
+            }
+            st.Index();
+            st.GenerateNormals();
+            return st.Commit();
+        }
+        (Vertex[] Draw, Vertex[] Collider) verts = GenerateVerts(
+            radius, markerPos, lodDist, planetNoise, noiseMult);
+        return (ProcessVertArray(verts.Draw), ProcessVertArray(verts.Collider));
+    }
+
+    public static (Vertex[] Draw, Vertex[] Collider) GenerateVerts(
+        double radius,
+        Vector3 markerPos,
+        double[] lodDist,
+        Noise planetNoise,
+        double noiseMult)
+    {
+        return ([new Vertex()], [new Vertex()]);
+    }
+}
